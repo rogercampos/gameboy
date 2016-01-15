0e1dbf579dde5c68b48b2456ddb892161487ec4d
Roger Campos <roger@itnig.net>
2016-01-15 21:08:43 +0100

    Registers

---
+++ README.md
@@ -0,0 +1,5 @@
+References:
+
+http://gbdev.gg8.se/files/docs/mirrors/pandocs.html
+http://www.pastraiser.com/cpu/gameboy/gameboy_opcodes.html
+http://imrannazar.com/content/files/jsgb.z80.js
---
+++ lib/registers.rb
@@ -0,0 +1,43 @@
+module Registers
+  extend self
+
+  %w(a f b c d e h l).each do |register|
+    define_method("#{register}=") do |value|
+      instance_variable_set "@#{register}", value % (2 ** 8)
+    end
+
+    define_method(register) do
+      instance_variable_get("@#{register}")
+    end
+  end
+
+  %w(sp pc).each do |register|
+    define_method("#{register}=") do |value|
+      instance_variable_set "@#{register}", value % (2 ** 16)
+    end
+
+    define_method(register) do
+      instance_variable_get("@#{register}")
+    end
+  end
+
+  %w(af bc de hl).each do |double_register|
+    define_method(double_register) do
+      (send(double_register[0]) << 8) + send(double_register[1])
+    end
+
+    define_method("#{double_register}=") do |value|
+      value = value % (2 ** 16)
+
+      send("#{double_register[1]}=", value % (2 ** 8))
+      send("#{double_register[0]}=", value >> 8)
+    end
+  end
+end
+
+Registers.af = 0
+Registers.bc = 0
+Registers.de = 0
+Registers.hl = 0
+Registers.sp = 0
+Registers.pc = 0
