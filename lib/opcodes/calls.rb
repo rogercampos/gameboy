8000a5e102f3f3c48a1655c7130bdbbfd936678c
Roger Campos <regor.field@gmail.com>
2016-01-31 20:32:48 +0100

    Add some instructions regarding sp

--- lib/opcodes/calls.rb
+++ lib/opcodes/calls.rb
@@ -1,3 +1,8 @@
 Instruction.define do
-  opcode(0xcd, 12, 3) {  }
+  opcode(0xcd, 12, 3) { Registers.sp -= 2; MMU.wwrite(Registers.sp, Registers.pc + 2); Registers.pc = MMU.wread(Registers.pc) }
+
+  opcode(0xc4, 12, 3) { Instruction[0xcd].call if Flags.z == 0 }
+  opcode(0xcc, 12, 3) { Instruction[0xcd].call if Flags.z == 1 }
+  opcode(0xd4, 12, 3) { Instruction[0xcd].call if Flags.c == 0 }
+  opcode(0xdc, 12, 3) { Instruction[0xcd].call if Flags.c == 1 }
 end
