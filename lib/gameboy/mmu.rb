ada68c43a1e65b7826dd0a295045f51e2052c54d
Roger Campos <roger@itnig.net>
2016-02-19 22:02:10 +0100

    Handle echo ram

--- lib/gameboy/mmu.rb
+++ lib/gameboy/mmu.rb
@@ -5,25 +5,37 @@ module Gameboy
     @data = Array.new(2 ** 16) { 0 }
 
     def bread(address, opts = {})
+      address = handle_echo(address)
       parse(@data[address], opts.fetch(:as, :unsigned), 1)
     end
 
     def wread(address, opts = {})
+      address = handle_echo(address)
       value = @data[address] << 8 + @data[address + 1]
       parse(value, opts.fetch(:as, :unsigned), 2)
     end
 
     def wwrite(address, value)
+      address = handle_echo(address)
       @data[address] = (value >> 8) % 256
       @data[address + 1] = value % 256
     end
 
     def bwrite(address, value)
+      address = handle_echo(address)
       @data[address] = value % 256
     end
 
 
     private
+
+    def handle_echo(address)
+      if address >= 0xc000 || address <= 0xde00
+        address + 8192
+      else
+        address
+      end
+    end
 
     def parse(value, type, size_in_bytes)
       case type
