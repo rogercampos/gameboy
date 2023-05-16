99fed09b5dbaacc65f6683f1114ce4cc246c2d18
Roger Campos <roger@rogercampos.com>
2023-05-16 19:41:02 +0200

    Some extra tests

--- lib/gameboy/opcodes/returns.rb
+++ lib/gameboy/opcodes/returns.rb
@@ -3,13 +3,13 @@ module Gameboy
     family(:return) do
       opcode(0xc9, 8, 1) { destination = MMU.wread(Registers.sp); Registers.sp += 2; Registers.pc = destination }
 
-      # opcode(0xc0, 8, 1) { Instruction[0xc9].run if Flags.z == 0 }
-      # opcode(0xc8, 8, 1) { Instruction[0xc9].run if Flags.z == 1 }
-      # opcode(0xd0, 8, 1) { Instruction[0xc9].run if Flags.c == 0 }
-      # opcode(0xd8, 8, 1) { Instruction[0xc9].run if Flags.c == 1 }
+      opcode(0xc0, 8, 1) { Flags.z == 0 ? Instruction[0xc9].run : nil }
+      opcode(0xc8, 8, 1) { Flags.z == 1 ? Instruction[0xc9].run : nil }
+      opcode(0xd0, 8, 1) { Flags.c == 0 ? Instruction[0xc9].run : nil }
+      opcode(0xd8, 8, 1) { Flags.c == 1 ? Instruction[0xc9].run : nil }
 
-      # TODO
-      # opcode(0xd9, 8, 1) { Instruction[0xc9].call;  }
+      # TODO missing enable interrupts
+      opcode(0xd9, 8, 1) { Instruction[0xc9].run  }
     end
   end
 end
